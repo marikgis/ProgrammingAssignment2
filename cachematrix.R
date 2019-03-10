aad35b6a78b7ab68fd953c1fd905bfe6b9d54428
Marat Yelemesov <marikgis1@gmail.com>
2019-03-10 18:06:16 -0400

    minor adjustments made to comments and the cache message

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,7 @@
 ## Write a short comment describing this function
 
 # Accepts matrix as a argument and saves the inverse in a cache (different session)
+# This function assumes that inverse for the input matrix exists
 
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
@@ -22,13 +23,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 
-# returns cached data
+# returns data from the cache (if it exists)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
         i <- x$getinverse()
         if (!is.null(i)) {
-                message("getting cached data")
+                message("retrieving from the cache")
                 return(i)
         }
         data <- x$get()
